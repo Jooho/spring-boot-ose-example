6e7e94db7da9f6cd6aa5ed9e83a6dfbd48dc9c33
root <root@master1.example.com>
2017-01-10 16:37:02 -0500

    test

--- spring-boot-app/src/main/java/hello/HelloController.java
+++ spring-boot-app/src/main/java/hello/HelloController.java
@@ -1,14 +1,20 @@
 package hello;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
     
+
+   @Value("${hello.message}")
+   private String message;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot! - It is " + message;
     }
     
 }
